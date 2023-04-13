3b0e16817ffafa912ab1512a453d7f4b9fe950ae
syahfadel <syahfadel@gmail.com>
2023-04-14 02:46:54 +0700

    create validator Before create or update

--- entities/user.go
+++ entities/user.go
@@ -1,9 +1,12 @@
 package entities
 
 import (
-	// "github.com/asaskevich/govalidator"
-	// "gorm.io/gorm"
+	"myGram/helpers"
 	"time"
+
+	"github.com/asaskevich/govalidator"
+
+	"gorm.io/gorm"
 )
 
 type User struct {
@@ -17,4 +20,28 @@ type User struct {
 	Comments     []Comment
 	CreatedAt    time.Time `json:"created_at"`
 	UpdatedAt    time.Time `json:"updated_at"`
+}
+
+func (u *User) BeforeCreate(tx *gorm.DB) (err error) {
+	_, errCreate := govalidator.ValidateStruct(u)
+	if errCreate != nil {
+		err = errCreate
+		return
+	}
+
+	u.Password = helpers.HashPass(u.Password)
+	err = nil
+	return nil
+}
+
+func (u *User) BeforeUpdate(tx *gorm.DB) (err error) {
+	_, errUpdate := govalidator.ValidateStruct(u)
+
+	if errUpdate != nil {
+		err = errUpdate
+		return
+	}
+
+	err = nil
+	return
 }
